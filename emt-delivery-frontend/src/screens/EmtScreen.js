b5feb2393ccbf366474370b3f3a83f16aa506b9c
ro1814 <rnsm14@gmail.com>
2022-03-02 11:19:07 +0100

    Download button added and styled

--- emt-delivery-frontend/src/screens/EmtScreen.js
+++ emt-delivery-frontend/src/screens/EmtScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import "../styles/EmtScreenStyles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
 export const EmtScreen = () => {
   return (
     <>
       <h1 className="emtTitle">EMT Reporting</h1>
-      <Container>
+      <Container size='xl'>
         <h2 className="emtSubtitle">Select upload type</h2>
         <Form>
           {["radio"].map((type) => (
@@ -15,7 +17,7 @@ export const EmtScreen = () => {
                 inline
                 className="checkBox"
                 label="Client Scope"
-                name="group1"
+                name="SlientScope"
                 type={type}
                 id={`inline-${type}-1`}
               />
@@ -23,18 +25,21 @@ export const EmtScreen = () => {
                 inline
                 className="checkBox"
                 label="Asset Manager"
-                name="group1"
+                name="assetManager"
                 type={type}
                 id={`inline-${type}-2`}
               />
             </div>
           ))}
         </Form>
+          <Form>
         <Form.Group controlId="formFileSm" className="mb-3">
           <Form.Label>Format: xls, Max size 5MB</Form.Label>
-          <Form.Control type="file" size="sm" />
-          
+          <Form.Control type="file" size="sm"/>
+        <Button type='submit' className=' btn rounded-circle dlBtn'><FontAwesomeIcon icon={faDownload} /></Button>
+
         </Form.Group>
+        </Form>
       </Container>
     </>
   );
